43f1f9efa3e3c4719e10f09fb41adc2991737009
William Torsett <william.torsett@gmail.com>
2020-04-24 21:28:39 +0100

    First Commit - Setting up the playground

---
+++ docker-setup/DockerConfig/requirements.R
@@ -0,0 +1,4 @@
+# try adding your required or favourite packages here
+install.packages(c("data.table"))
+install.packages(c("devtools", "dplyr", "igraph", "visNetwork", "sparkline", "magrittr"))
+install.packages("shiny", repos="https://cran.rstudio.com/")
---
+++ docker-setup/run_docker.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# This is s simple docker run command, broken up so you can read each bit
+# -d flag runs in detatched mode
+# use -it to start in interactive mode
+# --rm removes the container on exit
+
+#sudo docker run -d --rm \
+#    -p 28787:8787 \                         # map ports
+#    --name hello-world2 \                   # name container
+#    -e USERID=$UID \                        # you need to share a UID so you can write to mount file on host
+#    -e PASSWORD=SoSecret! \                   # set rstudio password - user is rstudio
+#    -v $DATA_DIR:/home/rstudio/Data \       # mount data directory to pick up changes or write to host
+#       rstudio/hello-world                  # the name of the image
+
+# simple one liner for command line copying
+
+#Stop existing container named rstudio
+docker stop rstudio
+
+#Rebuild the base image of the container named rstudio
+docker build --rm --force-rm -t rstudio/rstudio-kitematic .
+
+# Idnentify running volumes
+# docker volume ls
+# docker volume rm kitematic
+
+DATA_DIR=/mnt/data/black_swan
+# changed {$pwd} as I want to to use a specific location.
+
+# Convert rstudio-kitematic to rstudio-risk and remove Kitematic volume.
+sudo ./docker-copyedit.py FROM rstudio/rstudio-kitematic INTO rstudio/rstudio-risk -vv REMOVE VOLUME /home/rstudio/kitematic
+
+#Run the Docker Container rstudio/rstudio-risk
+docker run -d --rm -p 8787:8787 --name rstudio -e PASSWORD=password -e USERID=$UID -v $DATA_DIR:/home/rstudio/black_swan rstudio/rstudio-risk
+# removed sudo from line above as the userid is part of docker group
+
+# this is an example of running the container in interactive mode and logging into a bash shell
+# sudo docker run -it --rm  --name hello-world2 -e USERID=$UID -v $DATA_DIR:/home/rstudio/Data rstudio/hello-world  /bin/bash
+
+#remove the kitematic folder
+docker exec -d rstudio rm -rf /home/rstudio/kitematic
+
+docker image prune
+
